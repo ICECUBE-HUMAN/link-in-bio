0fa5fab5b64c4e36f175ff2696f938ca12b0ec37
coggiee <zentechie7@gmail.com>
2026-07-27 22:09:35 +0900

    Update not-found.tsx

--- apps/frontend/src/components/layout/states/not-found.tsx
+++ apps/frontend/src/components/layout/states/not-found.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useLocation } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import {
 	Empty,
@@ -7,30 +7,50 @@ import {
 	EmptyHeader,
 	EmptyTitle,
 } from "@/components/ui/empty";
+import { env } from "@/env";
 
 export default function NotFound() {
+	const { pathname } = useLocation();
+	const handle = decodeURIComponent(pathname.split("/")[1] ?? "");
+
 	return (
 		<section className="flex flex-1 items-center justify-center px-6 py-16 font-brand">
 			<Empty className="">
 				<EmptyHeader className="gap-0">
-					<EmptyTitle className="font-brand font-bold text-[96px]">
+					<EmptyTitle className="font-brand font-bold text-[96px] leading-tight">
 						404
 					</EmptyTitle>
-					<EmptyDescription className="max-w-sm">
-						The page you are looking for does not exist.
+					<EmptyDescription className="max-w-sm text-gray-bright text-base tracking-tight">
+					  Maybe this is your lucky break.
 						<br />
-						It may have existed for a moment, but maybe it's just resting now.
+						No one has claimed this handle yet. Grab it now!
 					</EmptyDescription>
 				</EmptyHeader>
-				<EmptyContent className="my-4">
-					<Button
-						render={<Link to="/" />}
-						nativeButton={false}
-						variant={"secondary"}
-						className={"rounded-xl"}
-					>
-						Back to home
-					</Button>
+        <EmptyContent className="max-w-2xs my-4 gap-2">
+          <div className="w-full rounded-lg bg-secondary/80 flex items-center justify-center h-12 px-18">
+            <span className="text-muted-foreground/80">{env.VITE_APP_DOMAIN}/</span>
+            <span>{handle}</span>
+          </div>
+          <div className="flex flex-col w-full">
+            <Button
+              render={<Link to="/log-in" />}
+              size={'lg'}
+              nativeButton={false}
+						  variant={"secondary"}
+              className={"max-w-md rounded-lg h-12"}
+            >
+  					  Grab it!
+  					</Button>
+  					<Button
+              render={<Link to="/" />}
+              size={'sm'}
+  						nativeButton={false}
+  						variant={"link"}
+  						className={"rounded-lg text-gray-bright font-normal"}
+  					>
+  			      or back to home
+  					</Button>
+          </div>
 				</EmptyContent>
 			</Empty>
 		</section>
