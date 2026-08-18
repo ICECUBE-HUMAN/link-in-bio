e6c48faa6e732f267c5936847201c39e7967e883
coggiee <zentechie7@gmail.com>
2026-08-18 17:09:17 +0900

    chore

--- apps/frontend/src/components/auth/cta-button.tsx
+++ apps/frontend/src/components/auth/cta-button.tsx
@@ -10,7 +10,7 @@ export default function CTAButton({
 		<Button
       size="lg"
       variant={'brand'}
-			className="rounded-xl w-full py-5.5 h-12 text-base md:text-lg md:h-14"
+			className="rounded-xl w-full py-5.5 h-13 text-lg md:text-lg md:h-14"
 			nativeButton={false}
 			render={<Link to="/log-in">{title}</Link>}
 		/>
--- apps/frontend/src/components/auth/demo-button.tsx
+++ apps/frontend/src/components/auth/demo-button.tsx
@@ -6,7 +6,7 @@ export default function TryDemoButton() {
     <Button
       size="lg"
       variant={'secondary'}
-			className="rounded-xl w-full py-5.5 h-12 text-base md:text-lg md:h-14 text-muted-foreground"
+			className="rounded-xl w-full py-5.5 h-13 text-lg md:text-lg md:h-14 text-muted-foreground"
 			nativeButton={false}
 			render={
 				<Link to="/demo">
