7c1dfb98790db642605418860b94f717094ae97a
coggiee <zentechie7@gmail.com>
2026-08-19 23:07:30 +0900

    init version 2 of frontend built with next.js

--- apps/v2/app/page.tsx
+++ apps/v2/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="mx-auto max-w-2xl space-y-8 p-8">
+      <div>
+        <Button size={"lg"} className={"font-bold text-base"}>
+          버튼
+        </Button>
+      </div>
     </main>
   );
 }
